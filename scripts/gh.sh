ddb2d2e8c1fe941423e565b27b330b587d206959
Nejat Onay Erköse <nejatonay.erkose@airties.com>
2018-08-16 11:07:42 +0300

    --
    Issue:RESYS

--- scripts/gh.sh
+++ scripts/gh.sh
@@ -3,11 +3,13 @@ cp ../.viminfo viminfo
 cp ../.vimrc vimrc
 
 git add *
+git status
 git commit -a -m $'\n\n--\n\nIssue:VIM \n\n'
 git push
 
 cd $HOME/DataDrive/reSystem
 git add *
+git status
 git commit -a -m $'\n\n--\n\nIssue:RESYS \n\n'
 git push
 
