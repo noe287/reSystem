870d617b68fb9995b720f50d960a7cd12d46cf20
Nejat Onay Erköse <nejatonay.erkose@airties.com>
2018-10-03 16:33:06 +0300

    $\n\n--\n\nIssue:RESYS \n\n

--- playground.c/binary/strtok_r.c
+++ playground.c/binary/strtok_r.c
@@ -7,11 +7,14 @@ int main(void)
     char *rest = NULL;
     char *token;
 
-    for (token = strtok_r(str, ",", &rest);
-         token != NULL;
-         token = strtok_r(NULL, ",", &rest)) {
+    for (token = strtok_r(str, ",", &rest); token != NULL; token = strtok_r(NULL, ",", &rest)) {
         printf("token:%s\n", token);
     }
 
     return 0;
 }
+
+/*   Name        Peer MAC        Peer Bridge MAC    Metric */
+/* ========  =================  ================= ========== */
+/*   wds1.2: 00:00:00:00:00:00  00:00:00:00:00:00       2941 */
+/*   wds1.1: 78:3e:53:ff:72:60  78:3e:53:ff:72:62       2500 */
