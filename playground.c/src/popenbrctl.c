5b7a666f5497172b32e27532d99e9f8496d659d6
Nejat Onay Erköse <nejatonay.erkose@airties.com>
2018-11-01 16:28:04 +0300

    $\n\n--\n\nIssue:RESYS \n\n

--- playground.c/src/popenbrctl.c
+++ playground.c/src/popenbrctl.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-/* # route -n */
-/* Kernel IP routing table */
-/* Destination     Gateway         Genmask         Flags Metric Ref    Use Iface */
-/* 0.0.0.0         172.16.0.1      0.0.0.0         UG    0      0        0 br0 */
-/* 172.16.0.0      0.0.0.0         255.255.0.0     U     0      0        0 br0 */
 
 int get_gw_ip(FILE *fptr, char *ifname, char *gw_ip);
 
-int main(){
+int main()
+{
+
 	FILE *fptr = NULL;
 	char gw_ip[16] = {"0.0.0.0"};
 	char *ifname ="enp5s0";
@@ -29,8 +26,6 @@ int get_gw_ip(FILE *fptr, char *ifname, char *gw_ip)
 {
   char line[1024] = {0};
   char *read = NULL;
-
-  /* fptr = popen("route -n", "r"); */
 
   while ((read = fgets(line, sizeof(line), fptr)) != NULL) {
 	  if (strstr(read, "UG") != NULL) {
