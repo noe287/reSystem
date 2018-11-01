d9d30d623d3a17d7bf1482084108544b0c2ba7b3
Nejat Onay Erköse <nejatonay.erkose@airties.com>
2018-11-01 16:24:04 +0300

    $\n\n--\n\nIssue:RESYS \n\n

--- playground.c/src/getGwIPRouteT.c
+++ playground.c/src/getGwIPRouteT.c
@@ -13,21 +13,12 @@ int main(){
 	FILE *fptr = NULL;
 	char gw_ip[16] = {"0.0.0.0"};
 	char line[1024] = {0};
-	char *read = NULL;
 	char *ifname ="enp5s0";
 	int ret = 0;
 
 	fptr = popen("route -n", "r");
 	ret = get_gw_ip(fptr, ifname, gw_ip);
 
-	/* while ((read = fgets(line, sizeof(line), fptr)) != NULL) { */
-	/* 	printf("rEAD:%s\n",read); */
-	/* 	if (strstr(read, "UG") != NULL)  */
-	/* 		if (strstr(read, "enp5s0") != NULL) { */
-	/* 			sscanf(read,"%*s %s", gw_ip); */
-	/* 			break; */
-	/* 		}	 */
-	/* } */
 	if(gw_ip != NULL)
 		printf("GWIP:%s\n",gw_ip);
 	return 0;
@@ -38,7 +29,7 @@ int get_gw_ip(FILE *fptr, char *ifname, char *gw_ip)
   char line[1024] = {0};
   char *read = NULL;
 
-  fptr = popen("route -n", "r");
+  /* fptr = popen("route -n", "r"); */
 
   while ((read = fgets(line, sizeof(line), fptr)) != NULL) {
 	  if (strstr(read, "UG") != NULL) {
