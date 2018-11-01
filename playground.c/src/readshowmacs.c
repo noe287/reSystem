d9e96f68761defac890f4075844216a4d76e2b71
Nejat Onay Erköse <nejatonay.erkose@airties.com>
2018-11-01 16:56:07 +0300

    $\n\n--\n\nIssue:RESYS \n\n

--- playground.c/src/readshowmacs.c
+++ playground.c/src/readshowmacs.c
@@ -1,19 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 
-#define FILE "../txt/showmacx.txt"
+#define NVRAM_FILE "nvram.txt"
 
-int main(){
-	FILE *fptr = NULL;
-	char line[1024] = {0};
-	char *red = NULL;
+int main()
+{
+        int error = 0 , ret = 0;
+        char* line;
+        char buf[128];
+        FILE* fds = NULL;
+	char serial_number[128] = {0};	
+	int len = 0;
 
-	fptr = fopen(FILE, "r");
-	red = fgets(line, sizeof(line), fptr);
-	line[strlen(line)-1] = '\0';
-	printf("%s\n", red);
-	printf("%s\n", line);
+        fds = fopen(NVRAM_FILE, "r");
 
-	return 0;
+
+        if (fds == NULL) {
+                printf("Cannot open nvram.txt file error: %s", strerror(error));
+                ret = -1;
+                goto out;
+        }
+
+        while ((line = fgets(buf, sizeof(buf), fds)) != NULL) {
+                /* if (strstr(line, "SERIAL_NUMBER") == NULL) { */
+                if (strstr(line, "MAC_BR_0") == NULL) {
+                        continue;
+                } else {
+			len = strlen (line) - 1;
+			if (line[len] == '\n')
+		                line[len] = '\0';
+                        printf("%s : %lu :%d\n", line, sizeof(buf), strlen(line));
+                        strncpy(serial_number, line + strlen("MAC_BR_0") + 1, 18);
+                        printf("%s\n", serial_number);
+                        break;
+                }
+        }
+
+out:
+        if (fds != NULL) {
+                fclose(fds);
+        }
 }
-
--- playground.c/src/readshowstp.c
+++ playground.c/src/readshowstp.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 
-#define FILED "wl_wps_configuuid_1"
+#define NVRAM_FILE "nvram.txt"
+
+int main()
+{
+        int error = 0 , ret = 0;
+        char* line;
+        char buf[128];
+        FILE* fds = NULL;
+	char serial_number[128] = {0};	
+	int len = 0;
+
+        fds = fopen(NVRAM_FILE, "r");
 
 
+        if (fds == NULL) {
+                printf("Cannot open nvram.txt file error: %s", strerror(error));
+                ret = -1;
+                goto out;
+        }
 
+        while ((line = fgets(buf, sizeof(buf), fds)) != NULL) {
+                /* if (strstr(line, "SERIAL_NUMBER") == NULL) { */
+                if (strstr(line, "MAC_BR_0") == NULL) {
+                        continue;
+                } else {
+			len = strlen (line) - 1;
+			if (line[len] == '\n')
+		                line[len] = '\0';
+                        printf("%s : %lu :%d\n", line, sizeof(buf), strlen(line));
+                        strncpy(serial_number, line + strlen("MAC_BR_0") + 1, 18);
+                        printf("%s\n", serial_number);
+                        break;
+                }
+        }
 
-int main(){
-	FILE *fptr = NULL;
-	char line[1024] = {0};
-	char *red = NULL;
-
-	fptr = fopen(FILED, "r");
-	red = fgets(line, sizeof(line), fptr);
-	line[strlen(line)-1] = '\0';
-	printf("%s\n", red);
-	printf("%s\n", line);
-
-	return 0;
+out:
+        if (fds != NULL) {
+                fclose(fds);
+        }
 }
-
