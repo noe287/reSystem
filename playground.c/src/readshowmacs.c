af5dc14ab2aa3f720a36b97ea6929a96fb155a7b
Nejat Onay Erköse <nejatonay.erkose@airties.com>
2018-11-01 17:07:04 +0300

    $\n\n--\n\nIssue:RESYS \n\n

--- playground.c/src/readshowmacs.c
+++ playground.c/src/readshowmacs.c
@@ -31,6 +31,22 @@ int main()
 			if (line[len] == '\n')
 		                line[len] = '\0';
 			printf("%s\n", line);
+			for (token = strtok_r(line, " ", &saveptr); token != NULL; token = strtok_r(NULL, " ", &saveptr)) {
+		                if(strstr(token,":") && !strstr(token, ".") && !strstr(token,"00:")){
+                		        if(get_bridge_mac == 0) {
+                                		get_bridge_mac = 1;
+		                                continue;
+                		        }
+	        	                get_bridge_mac = 0;
+        	        	        if(ether_aton_r(token, &mac)){
+                               		/* if(memcmp(mac.ether_addr_octet, zero_mac, ETH_ALEN)) */
+	                                        printf("token:%s: MAC:"MACSTR"\n", token, MAC2STR(mac.ether_addr_octet));
+        	                                /* printf("token:%s: MAC:%02x\n", token, mac.ether_addr_octet[0]); */
+                        		}
+                		}
+        		}
+
+
                         /* printf(" %s : %lu :%lu \n", line, sizeof(buf), strlen(line)); */
                         /* strncpy(serial_number, line + strlen("MAC_BR_0") + 1, 18); */
                         /* printf("%s\n", serial_number); */
